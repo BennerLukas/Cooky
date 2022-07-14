1eed6eed973c7ac2ba142468eed4eaac0545e2ae
Madhour <aymanmadhour@gmail.com>
2022-07-14 19:19:16 +0200

    add minimum responvie web app

---
+++ app/cooky/src/views/Explore.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="explore pa-6">
+    <h1>Explore</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Explore',
+    components: {
+    },
+  }
+</script>
---
+++ app/cooky/src/views/Settings.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="settings pa-6">
+    <h1>Settings</h1>
+    U
+  </div>
+</template>
