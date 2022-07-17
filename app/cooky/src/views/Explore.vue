95b8362097c8ba6e48408b4a34eec468843b2f6f
Madhour <aymanmadhour@gmail.com>
2022-07-17 17:09:44 +0200

    connect client to api [WIP]

--- app/cooky/src/views/Explore.vue
+++ app/cooky/src/views/Explore.vue
@@ -163,6 +163,18 @@
 </template>
 
 <script>
+import axios from 'axios'
+
+async function getExploreData() {
+	try {
+		const response = await axios.get("http://localhost:5000/explore?reco=pantry");
+		console.log(response);
+    return response;
+	}
+	catch (error) {
+		console.log(error);
+	}
+}
 
   export default {
     name: 'Explore',
@@ -183,6 +195,8 @@
           { id: 7, title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 7 },
           { id: 8, title: 'Train', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 5 }
         ],//dynamically create cards by loading from API
+        response_cooky: {"n_recipe_id":{},"s_recipe_title":{},"array_ingredients":{},"s_directions":{},"s_link":{},"s_source":{},"array_NER":{}},//response json -> has all information needed for detail page as well
+
         detail: {
           title: 'Dish Title', src: 'https://cdn.vuetifyjs.com/images/cards/train.jpg', description: 'Description for this dish', recipe: 'Recipe for this dish', rating: 5}
       }
@@ -192,11 +206,16 @@
       itemClick(id) {
         this.dialog = true
         this.detail.title = this.cards.filter(dish => dish.id === id)[0].title //sample of changing titles
-        console.log(this.detail.title)
+        this.detail.src = this.cards.filter(dish => dish.id === id)[0].src //sample of changing titles
+        this.detail.description = this.cards.filter(dish => dish.id === id)[0].directions //sample of changing titles
+        this.detail.recipe = this.cards.filter(dish => dish.id === id)[0].recipe //sample of changing titles
         //fetch description and recipe from db to present
         // update detail object
         //this.detail = {fetched object}
       }
     }
   }
+
+cooky_response = getExploreData()
+this.cards = cooky_response
 </script>
--- app/cooky/src/views/Settings.vue
+++ app/cooky/src/views/Settings.vue
@@ -1,9 +1,63 @@
 
 <template>
+<v-form v-model="valid">
+    <h1>Login</h1>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="formData.username"
+            label="username"
+            required
+          ></v-text-field>
+        </v-col>
 
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="formData.password"
+            label="password"
+            required
+
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            
+            
+            @click:append="show = !show"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-btn @click="createPost">Login</v-btn>
+  </v-form>
 
 
 </template>
 
 <script>
+import axios from 'axios'
+
+  export default {
+    data: () => ({
+        name:"login",
+        formData : {
+            username: '',
+            password: '',
+        },
+      valid: false,
+      show: false,
+    }),
+    methods: {
+        createPost() {
+            axios.post('http://localhost:5000/login', this.formData)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+        }
+    }
+  }
 </script>
