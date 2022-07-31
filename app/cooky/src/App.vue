adb76d0c66c2d33cbb9261f31628e6a6ba51fd66
Madhour <aymanmadhour@gmail.com>
2022-07-31 21:50:12 +0200

    added comments

--- app/cooky/src/App.vue
+++ app/cooky/src/App.vue
@@ -1,43 +1,39 @@
 <template>
-  <v-app id="cooky">
-
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-
-
-    <v-bottom-navigation
-      app
-      :value="value"
-      color="primary"
-    >
+    <v-app id="cooky">
     
-      <v-btn
-        v-for="item in items"
-        :to="item.to">
-
-        <span>{{ item.title }}</span>
-
-        <v-icon>{{ item.icon }}</v-icon>
-        
-      </v-btn>
-      
-    </v-bottom-navigation>
-
-  </v-app>
+    
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    
+        <!-- Bottom Navigation Bar -->
+        <v-bottom-navigation app :value="value" color="primary">
+    
+            <v-btn v-for="item in items" :to="item.to">
+    
+                <span>{{ item.title }}</span>
+    
+                <v-icon>{{ item.icon }}</v-icon>
+    
+            </v-btn>
+    
+        </v-bottom-navigation>
+    
+    </v-app>
 </template>
 
 <script>
-  
-  export default {
+export default {
     data() {
-      return {
-    value: "1",
-    items: [
-          { title: 'Explore', icon: 'mdi-chef-hat', to:'/' },
-          { title: 'Pantry', icon: 'mdi-fridge', to:'/pantry' },
-          { title: 'Settings', icon: 'mdi-account', to:'/settings' },
-        ] }},// navigation items
-  }
+        return {
+            value: "1",
+            // Navigation Items
+            items: [
+                { title: 'Explore', icon: 'mdi-chef-hat', to: '/' },
+                { title: 'Pantry', icon: 'mdi-fridge', to: '/pantry' },
+                { title: 'Settings', icon: 'mdi-account', to: '/settings' },
+            ]
+        }
+    },
+}
 </script>
